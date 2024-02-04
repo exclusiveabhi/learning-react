79f11c0a2afc4400d0b9be2d2199910334e2e713
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-02-04 19:04:17 +0530

    04-02-2024

--- tute/src/App.js
+++ tute/src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    
    <div className='navbar'>
-   
        <ul>
-        {/* <li> <p className='namelogo'>Abhishek</p></li> */}
         <li>Home</li>
         <li>Project</li>
         <li>Github</li>
